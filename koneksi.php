6c25d7c7fd29f0d1f47d1dcc3cfd6c6e426df27a
Aderizqy <aderizqy99@gmail.com>
2024-06-11 10:13:06 +0700

    mengubah koneksi

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect("localhost", "id22298560_aderizqy", "Aderizqy123.", "id22298560_pw2024_tubes_233040008");
+$conn = mysqli_connect("localhost", "root", "", "pw2024_tubes_233040008");
 
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
